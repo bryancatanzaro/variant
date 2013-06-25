5bea333395784c4e5078a889576659d3bf1c18ab
Bryan Catanzaro <bcatanzaro@acm.org>
2013-06-25 16:02:07 -0700

    Add nested test.

---
+++ tests/nested.cu
@@ -0,0 +1,40 @@
+#include <variant/variant.h>
+#include <iostream>
+
+struct foo{};
+struct bar{};
+
+typedef variant::variant<foo, bar> variant_0;
+typedef variant::variant<variant_0, int> variant_1;
+
+struct variant_0_printer : public variant::static_visitor<> {
+    void operator()(const foo& x) const {
+        std::cout << "foo";
+    }
+    void operator()(const bar& x) const {
+        std::cout << "bar";
+    }
+};
+
+struct variant_1_printer : public variant::static_visitor<> {
+    void operator()(const variant_0& x) const {
+        std::cout << "variant_0: ";
+        variant::apply_visitor(variant_0_printer(), x);
+    }
+    void operator()(const int& x) const {
+        std::cout << "int: " << x;
+    }
+
+};
+
+
+
+int main() {
+    variant_1 x(1);
+    variant::apply_visitor(variant_1_printer(), x); std::cout << std::endl;
+    x = foo();
+    variant::apply_visitor(variant_1_printer(), x); std::cout << std::endl;
+    x = bar();
+    variant::apply_visitor(variant_1_printer(), x); std::cout << std::endl;
+
+}
